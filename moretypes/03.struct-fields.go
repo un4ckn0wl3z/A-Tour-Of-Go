a7bd9285d9e92aed7270ea0794e601c6421f8ade
u n 4 c k n 0 w l 3 z <gh0stsync.rt@gmail.com>
2019-08-07 18:07:30 +0700

    Create 03.struct-fields.go

---
+++ moretypes/03.struct-fields.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func main() {
+	v := Vertex{1, 2}
+	v.X = 4
+	fmt.Println(v.X)
+}
+/** 
+Struct Fields
+Struct fields are accessed using a dot.
+*/
