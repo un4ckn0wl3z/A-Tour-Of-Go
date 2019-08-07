8629a47dd130136ce213797eaab3c817839d5d8e
u n 4 c k n 0 w l 3 z <gh0stsync.rt@gmail.com>
2019-08-07 14:36:03 +0700

    Create 6.multiple-results.go

---
+++ 6.multiple-results.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func main() {
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+}
+
+
+/** 
+Multiple results
+A function can return any number of results.
+
+The swap function returns two strings.
+*/
